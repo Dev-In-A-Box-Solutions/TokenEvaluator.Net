ca754ba8723c84f4d662e259d42e58521027d9a9
Joe <hello@joetomkinson.dev>
2023-07-09 13:06:37 +0100

    Adjusted some of the tests.

--- README.md
+++ README.md
@@ -14,3 +14,47 @@ Whether you're running an AI chatbot, a content generator, or any application th
 5. **Open-source**, promoting community contributions and continuous improvement
 
 Unlock the power of accurate token count understanding with TextTokenizationEvaluator - the essential tool for AI developers.
+
+# Getting Started
+
+TextTokenizationEvaluator can be used via dependency injection, or an instance can be created using a tightly-coupled factory class.
+
+### Dependency Injection
+If you want to be able to inject an instance of this client into multiple methods, then you can make use of the libraries dependency injection extension to add all of the required interfaces and implementations to your service collection.
+
+```C#
+using TextTokenizationEvaluator.Core.Dependency;
+
+// Init a service collection, use the extension method to add the library services.
+IServiceCollection services = new ServiceCollection();
+services.AddTextTokenizationEvaluatorServices();
+services.AddSingleton<ITokenEvaluatorClient, TokenEvaluatorClient>();
+var serviceProvider = services.BuildServiceProvider();
+```
+
+Then simply inject the service into your class constructors like so:
+
+```C#
+internal const string GeneratedText = "The quick, brown fox—enamored by the moonlit night—jumped over 10 lazily sleeping dogs near 123 Elm St. at approximately 7:30 PM. Isn't text tokenization interesting?";
+
+public ClassConstructor(ITokenEvaluatorClient tokenClient)
+{
+    // Set token encoding type
+    tokenClient.SetDefaultTokenEncoding(EncodingType.Cl100kBase);
+    var tokenCount = tokenClient.EncodedTokenCount(GeneratedText);
+
+    // or choose a supported model
+    tokenClient.SetDefaultTokenEncodingForModel(ModelType.TextDavinci003);
+    var tokenCount = tokenClient.EncodedTokenCount(GeneratedText);
+}
+```
+
+### Factory Implementation
+
+Using this as a concrete, tightly-coupled implementation is fairly straightforward. Simply use the below code and all internal interface and service references will be initialised and tightly-coupled. This is difficult to write tests for within your application, but ultimately is the easiest way to implement the client.
+
+```C#
+var client = TokenEvaluatorClientFactory.Create();
+client.SetDefaultTokenEncoding(EncodingType.Cl100kBase);
+var tokenCount = client.EncodedTokenCount(Constants.GeneratedText);
+```
--- src/TextTokenizationEvaluator.Tests/EncoderTests.cs
+++ src/TextTokenizationEvaluator.Tests/EncoderTests.cs
@@ -18,7 +18,7 @@ namespace TextTokenizationEvaluator.Tests
         }
 
         [TestMethod]
-        public void TestC100kBase()
+        public void TestC100kBaseUsingEncodingType()
         {
             if (serviceProvider == null)
             {
@@ -40,7 +40,7 @@ namespace TextTokenizationEvaluator.Tests
         }
 
         [TestMethod]
-        public void TestP50kBase()
+        public void TestP50kBaseUsingEncodingType()
         {
             if (serviceProvider == null)
             {
@@ -54,6 +54,50 @@ namespace TextTokenizationEvaluator.Tests
                 tokenClient.SetDefaultTokenEncoding(EncodingType.P50kBase);
                 var tokenCount = tokenClient.EncodedTokenCount(Constants.GeneratedText);
                 Assert.AreEqual(tokenCount, 42);
+            }
+            else
+            {
+                Assert.Fail("Token Client Null");
+            }
+        }
+
+        [TestMethod]
+        public void TestTextDavinci003UsingModelType()
+        {
+            if (serviceProvider == null)
+            {
+                Assert.Fail("Service Provider Null");
+            }
+
+            // test we can retrieve the references to the interfaces.
+            var tokenClient = serviceProvider.GetService<ITokenEvaluatorClient>();
+            if (tokenClient != null)
+            {
+                tokenClient.SetDefaultTokenEncodingForModel(ModelType.TextDavinci003);
+                var tokenCount = tokenClient.EncodedTokenCount(Constants.GeneratedText);
+                Assert.AreEqual(tokenCount, 42);
+            }
+            else
+            {
+                Assert.Fail("Token Client Null");
+            }
+        }
+
+        [TestMethod]
+        public void TestGpt35TurboUsingModelType()
+        {
+            if (serviceProvider == null)
+            {
+                Assert.Fail("Service Provider Null");
+            }
+
+            // test we can retrieve the references to the interfaces.
+            var tokenClient = serviceProvider.GetService<ITokenEvaluatorClient>();
+            if (tokenClient != null)
+            {
+                tokenClient.SetDefaultTokenEncodingForModel(ModelType.Gpt3_5Turbo);
+                var tokenCount = tokenClient.EncodedTokenCount(Constants.GeneratedText);
+                Assert.AreEqual(tokenCount, 45);
             }
             else
             {
